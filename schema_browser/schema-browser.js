9a5e1388eff83878c66e2ea1a6c25a864fd37acf
Dung Truong <dt.young112@gmail.com>
2023-04-13 10:03:34 -0700

    add library schemas to prerelease viewer

--- schema_browser/schema-browser.js
+++ schema_browser/schema-browser.js
@@ -26,9 +26,26 @@ function load(schema_name) {
     standard_schema_api_path = github_endpoint + "/standard_schema";
     library_schema_api_path = github_endpoint + "/library_schemas";
     if (schema_name.includes('prerelease')) {
-        var schema_link = getPrereleaseXml(standard_schema_api_path + "/prerelease");
-        // load default schema accordingly
+        var name_without_prerelease = schema_name.replace('_prerelease', '');
+        if (name_without_prerelease == "standard") {
+            var schema_link = getPrereleaseXml(standard_schema_api_path + "/prerelease");
+        }
+        else {
+            var schema_link = getPrereleaseXml(library_schema_api_path + "/" + name_without_prerelease + "/prerelease");
+        }
+        // load preprelease schema accordingly
         loadSchema(schema_link)
+
+        // add schema names to schema dropdown button
+        var standard_prerelease_schema_link = getPrereleaseXml(standard_schema_api_path + "/prerelease");
+        var html = '<a class="dropdown-item" id="schemaStandard" + " onclick="loadSchema(\'' + standard_prerelease_schema_link + '\')">Standard</a>';
+        $("#schemaDropdown").append(html);
+        library_schemas = getLibarySchemas();
+        for (var i=0; i < library_schemas.length; i++) {
+            var library_schema_link = getPrereleaseXml(library_schema_api_path + "/" + library_schemas[i] + "/prerelease"); 
+            var html = '<a class="dropdown-item" id="schemaStandard" + " onclick="loadSchema(\'' + library_schema_link + '\')">' + library_schemas[i] + '</a>';
+            $("#schemaDropdown").append(html);
+        }
     }
     else {
         // add schema names to schema dropdown button
@@ -36,7 +53,7 @@ function load(schema_name) {
         $("#schemaDropdown").append(html);
         library_schemas = getLibarySchemas();
         for (var i=0; i < library_schemas.length; i++) {
-            var html = '<a class="dropdown-item" id="schemaStandard" + " onclick="loadDefaultSchema(\'' + library_schemas[i] + '\')">' + library_schemas[i].toUpperCase() + '</a>';
+            var html = '<a class="dropdown-item" id="schemaStandard" + " onclick="loadDefaultSchema(\'' + library_schemas[i] + '\')">' + library_schemas[i] + '</a>';
             $("#schemaDropdown").append(html);
         }
         
@@ -462,15 +479,16 @@ function getSchemaNodes() {
 	schemaNodes.push($(this).attr("tag"));
     });    
     
-    /* add autocomplete and search */
-    $( function() {
-    $( "#searchTags" ).autocomplete({
-      source: schemaNodes,
-      select: function(event, ui) {
-	toNode(ui.item.value);
-      }
-    });
-  } );
+//     /* add autocomplete and search */
+//     $( function() {
+//     $( "#searchTags" ).autocomplete({
+//       source: schemaNodes,
+//       select: function(event, ui) {
+// 	toNode(ui.item.value);
+//       }
+//     });
+//   } );
+    autocomplete(document.getElementById("searchTags"), schemaNodes);
     /* search on enter key press */
     $("#searchTags").on('keyup', function (e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
@@ -500,3 +518,111 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+/**
+ * From autocomplete online tutorial
+ */
+function autocomplete(inp, arr, suggested_tags_arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>SUGGESTED TAGS</strong>";
+        a.appendChild(b)
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "Test";
+        a.appendChild(b)
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+  } 
