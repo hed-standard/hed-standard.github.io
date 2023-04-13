0b0765b5d98ab68eb267f39bb1ba54861a6af187
Dung Truong <dt.young112@gmail.com>
2023-04-13 10:16:34 -0700

    add long form text

--- schema_browser/schema-browser.js
+++ schema_browser/schema-browser.js
@@ -391,7 +391,8 @@ function infoBoardMouseoverEvent(event) {
             finalText = finalText == null || finalText.length == 0 ? "" : finalText;
         var disp_div = ["schemaNode", "unitClassDef", "unitModifierDef", "valueClassDef", "attributeDef", "propertyDef"];
         if (disp_div.includes(selected.attr('name'))) {
-            $("h4#title").text(path);
+            $("h4#title").text(node.text());
+            $("p#tag").text("Long form: " + path);
             $("p#description").text(selected.attr("description"));
             $("div#attribute_info").html(finalText);
         }
