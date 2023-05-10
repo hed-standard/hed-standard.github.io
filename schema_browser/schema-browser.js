30e4d547d03ba550cf5b20113b4f155759b5f3c9
Dung Truong <dt.young112@gmail.com>
2023-05-10 14:56:27 -0700

    search for word containing key

--- schema_browser/schema-browser.js
+++ schema_browser/schema-browser.js
@@ -587,12 +587,13 @@ function autocomplete(inp, arr, suggestedTagsDict) {
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toLowerCase() == val) {
+          if (arr[i].toLowerCase().includes(val)) {
             /*create a DIV element for each matching element:*/
             b = document.createElement("DIV");
             /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
+            b.innerHTML = arr[i].substr(0, arr[i].toLowerCase().indexOf(val));
+            b.innerHTML += "<strong>" + arr[i].substr(arr[i].toLowerCase().indexOf(val), val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(arr[i].toLowerCase().indexOf(val)+val.length);
             /*insert a input field that will hold the current array item's value:*/
             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
             /*execute a function when someone clicks on the item value (DIV element):*/
